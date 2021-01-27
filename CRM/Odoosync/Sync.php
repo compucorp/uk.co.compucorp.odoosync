0cdbb0be2764086cc011dcfd0809881bf440b43c
Erawat Chamanont <erawat@gmail.com>
2021-01-27 09:38:59 +0000

    ASESPRT-311: Write debug log into file when debug param is passed to the API

--- CRM/Odoosync/Sync.php
+++ CRM/Odoosync/Sync.php
@@ -21,16 +21,19 @@ class CRM_Odoosync_Sync {
 
     $log['is_error'] = ($logContact['is_error'] == 1 || $logContact['is_error'] == 1) ? 1 : 0;
 
-    //more detail log can view in api when debug = 1
+    //this log can view on schedule job
+    $log['values'] = '<br/>' . $logContact['values'] . $logContribution['values'];
+    $log['values'] .= !empty($mailLog) ? '<br/>' . $mailLog : '';
+
+    //View more detail log in api explorer
+    //and write debug log into CiviCRM log
+    //when debug = 1 is passed as parameter
     if ($params['debug'] == 1) {
       $log['debugLog']['contacts_debug_log'] = $logContact['debugLog'];
       $log['debugLog']['contribution_debug_log'] = $logContribution['debugLog'];
       $log['debugLog']['mail_debug_log'] = $mailLog;
+      Civi::log()->debug(print_r($log['debugLog'],true));
     }
-
-    //this log can view on schedule job
-    $log['values'] = '<br/>' . $logContact['values'] . $logContribution['values'];
-    $log['values'] .= !empty($mailLog) ? '<br/>' . $mailLog : '';
 
     return $log;
   }
